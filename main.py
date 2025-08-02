d42cadd9ea091e1c4b4c8f063711baa3ebb6e08e
Tanishk Raj <tanishk.r@tataelxsi.co.in>
2025-08-02 16:55:44 +0530

    Added main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+# import psutil
+
+# def mem_prof_cpu():
+#     """Memory Profiler for CPU"""
+#     process = psutil.Process()
+#     cpu_mem =process.memory_info().rss
+#     mem_used_by_task = round((cpu_mem)/(1024*1024))
+#     percent_mem_usage = process.memory_percent()
+#     return mem_used_by_task,percent_mem_usage
+
+# value1 ,value2=mem_prof_cpu()
+# print(value1, value2*100)
+import psutil
+
+class MyTask:
+
+    @classmethod
+    def mem_prof_cpu(cls):
+        """Memory Profiler for CPU"""
+        process=psutil.Process()
+        cpu_mem =process.memory_info().rss
+        mem_used_by_task = round((cpu_mem)/(1024*1024))
+        percent_mem_usage = process.memory_percent()
+        return mem_used_by_task,percent_mem_usage
+    
+def normal_function():
+    result,percent = MyTask.mem_prof_cpu()
+    print(result,percent*100)
+
+if __name__ == '__main__':
+    normal_function()
